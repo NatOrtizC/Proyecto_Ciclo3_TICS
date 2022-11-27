63ccdcd1f5cd0688b3c376c4d38f5ae843483db0
NatOrtizC <natyorticruz@gmail.com>
2022-11-26 22:12:12 -0500

    Archivo descartado

--- proyect_backesecurity/routes/admins.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const adminsController = require("../controllers/admins.controller")
-
-router.post("/login", adminsController.login)
-
-
-module.exports = router
